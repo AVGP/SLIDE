f2358cd94ec9b956dca5ab0a33cb7695583121c0
Martin Naumann <avgp_2k@yahoo.de>
2010-12-25 17:42:50 +0100

    Porting the Starter to Qt.

--- Apps/Starter/main.cpp
+++ Apps/Starter/main.cpp
@@ -1,16 +1,16 @@
-#include "main.h"
+#include <QApplication>
+#include <iostream>
 #include "starter.h"
 
-IMPLEMENT_APP(StarterApp);
-
-bool StarterApp::OnInit()
+int main(int argc, char **argv)
 {
-    if(argc < 3) return false;
-
-    int w = atoi(argv[1]);
-    int h = atoi(argv[2]);
-
-    Starter *s = new Starter(w,h,"Starter");
-    s->Show(true);
-    return true;
+    if(argc < 3)
+    {
+        std::cout << "Usage: Starter <Width> <Height>" << std::endl;
+        return 1;
+    }
+    QApplication app(argc,argv);
+    Starter *s = new Starter(atoi(argv[1]),atoi(argv[2]));
+    s->show();
+    return app.exec();
 }
