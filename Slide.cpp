4dc5cad4a894a64337475c3513397ee6124b3c28
Martin Naumann <avgp_2k@yahoo.de>
2010-12-25 01:34:12 +0100

    Fixed the execl-Call for the desk.

--- Slide.cpp
+++ Slide.cpp
@@ -73,7 +73,7 @@ bool Slide::startUp(bool debug)
 //        componentPIDs[2+i] = fork();
         if(fork() == 0)//componentPIDs[2+i] == 0)
         {
-            execl((char *)config->getConfigValue((char *)"DesktopApp",&len),(char *)"SlideComponent",sw,sh,(char *)config->getConfigValue((char *)"DesktopWallpaper",&len),(char *)0);
+            execl((char *)config->getConfigValue((char *)"DesktopApp",&len),(char *)"Desk",sw,sh,(char *)config->getConfigValue((char *)"DesktopWallpaper",&len),NULL);
         }
     }
 
